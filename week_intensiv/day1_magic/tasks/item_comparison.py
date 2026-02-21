2a40672046f14803e3087d912da38dbb06e4ab81
nikitauzbekov280707-cmd <nikita.uzbekov280707@gmail.com>
2026-02-21 14:36:08 +0300

    inittttt

--- week_intensiv/day1_magic/tasks/item_comparison.py
+++ week_intensiv/day1_magic/tasks/item_comparison.py
@@ -1,5 +1,5 @@
 class Item:
     """ЗАДАЧА: Сравнение товаров по цене через __lt__ и по всем полям через __eq__"""
     def __init__(self, name, price): self.name, self.price = name, price
-    def __lt__(self, other): pass
-    def __eq__(self, other): pass
+    def __lt__(self, other): return self.price < other.price
+    def __eq__(self, other): return self.price == other.price
