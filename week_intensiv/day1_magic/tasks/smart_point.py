961c8e580b251ed67bbb8842fcd8c98547ef9000
nikitauzbekov280707-cmd <nikita.uzbekov280707@gmail.com>
2026-02-21 14:45:58 +0300

    inittttt

--- week_intensiv/day1_magic/tasks/smart_point.py
+++ week_intensiv/day1_magic/tasks/smart_point.py
@@ -1,5 +1,5 @@
 class SmartPoint:
     """ЗАДАЧА: Реализовать __str__ (Точка(x, y)) и __repr__ (SmartPoint(x=x, y=y))"""
     def __init__(self, x, y): self.x, self.y = x, y
-    def __str__(self): pass
-    def __repr__(self): pass
+    def __str__(self): return f'N=Точка({self.x}, {self.y})'
+    def __repr__(self): return f"SmartPoint(x={self.x}, y={self.y})"
--- week_intensiv/day1_magic/tasks/wallet.py
+++ week_intensiv/day1_magic/tasks/wallet.py
@@ -1,5 +1,5 @@
 class Wallet:
     """ЗАДАЧА: Сложение кошельков через __add__ (новый Wallet) и длина через __len__ (целый баланс)"""
     def __init__(self, name, balance): self.name, self.balance = name, balance
-    def __add__(self, other): pass
-    def __len__(self): pass
+    def __add__(self, other): return Wallet(self.name, self.balance + other.balance)
+    def __len__(self): return len(self.name)
